d3fec32208a6d53d154d1dde674b33bc62535a62
Jonathan Perret <j-github@jonathanperret.net>
2021-12-04 09:35:34 +0100

    run_tests.sh: allow specifying file list

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-for testfile in $(find . -name '*_tests.tal'); do
+testlist=${1:-$(find . -name '*_tests.tal')}
+
+for testfile in $testlist; do
   uxnasm $testfile ${testfile%.tal}.rom 2>&1 | (egrep -v 'Unused label|^Assembled' ||:)
 
   diff -U 2 <(</dev/null uxncli ${testfile%.tal}.rom) ${testfile%.tal}.expected
