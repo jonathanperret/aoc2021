91ad2c4a42f3631949d8bb7828187ddc8fffdbf5
Jonathan Perret <j-github@jonathanperret.net>
2021-12-04 09:36:04 +0100

    run_tests.sh: allow embedded expected output

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,13 @@ testlist=${1:-$(find . -name '*_tests.tal')}
 for testfile in $testlist; do
   uxnasm $testfile ${testfile%.tal}.rom 2>&1 | (egrep -v 'Unused label|^Assembled' ||:)
 
-  diff -U 2 <(</dev/null uxncli ${testfile%.tal}.rom) ${testfile%.tal}.expected
+  diff -U 2 <(
+    if [ -f ${testfile%.tal}.expected ]; then
+      cat ${testfile%.tal}.expected
+    else
+      < $testfile awk '/\( EXPECT/ { sub(".*\\( EXPECT ",""); sub(" \\)$",""); print; }'
+    fi
+  ) <(</dev/null uxncli ${testfile%.tal}.rom)
 done
 
 echo "All OK!"
